b991910bb63ebb758b43681c8f13902fa16cefd5
Maurice Nicholson <maurice@mauricenicholson.com>
2012-07-25 21:32:24 +0100

    Initial commit

---
+++ app/app.go
@@ -0,0 +1,17 @@
+// Copyright Maurice Nicholson. All rights reserved.
+
+package app
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.Redirect(w, r, "/", http.StatusMovedPermanently)
+			return
+		}
+		http.ServeFile(w, r, "static/index.html")
+	})
+}
